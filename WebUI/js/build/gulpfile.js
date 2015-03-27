1457acc9f128cd968bab578f170641669b4cb4e7
seantarogers <seantarogers@yahoo.co.uk>
2015-03-27 07:52:52 +0000

    added gulp file

---
+++ WebUI/js/build/gulpfile.js
@@ -0,0 +1,29 @@
+﻿var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var jshintstylish = require('gulp-jscs');
+
+gulp.task('vetMyCode', function() {
+    gulp.src([
+            '../actions/*.js',
+            '../components/*.js',
+           '../constants/*.js',
+           '../dispatcher/*.js',
+           '../repositories/*.js',
+           '../stores/*.js',
+           '../app.js'
+        ])
+        .pipe(jshint())
+        .pipe(jscs())
+        .pipe(jshint.reporter('jshint-stylish',{verbose: true}));
+});
+//gulp.task('hello-world-task', function () {
+//    console.log("our first hello world task");
+//});
+
+//'./components/*.js',
+//           './constants/*.js',
+//           './dispatcher/*.js',
+//           './repositories/*.js',
+//           './stores/*.js',
+//           './app.js'
